e911d956f98b2075d234a6ff95f5c0efa63d2094
Oscar Stenström <93720714+ostenst@users.noreply.github.com>
2025-05-08 16:37:21 +0200

    Development

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 # This is the model.py file
 import numpy as np
 import pandas as pd
-
+from sklearn.linear_model import LinearRegression
 
 def plan_CCU(plant):
     CCU = 1
@@ -45,8 +45,16 @@ def plan_CCS(plant,x):
     # estimate CAPEX and construct a bid of CAPEX+transport+storage
     CAPEX_CC = 4121.7 * Vfluegas ** 0.6498          #[kEUR]
     CAPEX_CL = 7004.6 * mcaptured ** 0.5243         #[kEUR] NOTE only compression cost
+    print(Vfluegas)
     print(CAPEX_CC)
     print(CAPEX_CL)
+    CAPEX = x["FOAK"] * (CAPEX_CC + CAPEX_CL)
+    print(CAPEX)
+
+    CRF = (x["dr"] * (1 + x["dr"]) ** x["t"]) / ((1 + x["dr"]) ** x["t"] - 1)
+    annualized_CAPEX = CAPEX * CRF
+    levelized_CAPEX = annualized_CAPEX / (mcaptured/1000*3600 * x["FLH"])
+    print(levelized_CAPEX*1000)
 
     FCCS = 1
     BECCS = 1
@@ -73,6 +81,15 @@ def WACCUS_EPR(
     qreb = 3.5,         #[MJ/kgCO2]
     COP = 3,
 
+    FLH = 8000,
+    dr = 0.075,
+    t = 25,
+    FOAK = 2.20,        #[-] [Beiron, 2024] applies to CO2 capture and conditioning 
+    k = 0.6857,         #[-] [Stenström, 2025]
+
+    RENOVA = ["FOAK", "NOAK"],
+    HANDELO = ["FOAK", "NOAK"], # etc. ... matters most!
+
     # levers 
     tax = [800,1600,2400,3200],
     groups = [0,1,2,3],
@@ -95,6 +112,11 @@ def WACCUS_EPR(
         "fossil": fossil,
         "qreb": qreb,
         "COP": COP,
+        "FLH": FLH,
+        "dr": dr,
+        "t": t,
+        "FOAK": FOAK,
+        "k": k,
         "tax": tax,
         "groups": groups
     }
@@ -127,7 +149,21 @@ def WACCUS_EPR(
     return output
 
 if __name__ == "__main__":
+    # finding k exponent of CAPEX=CAPEX_ref⋅(mCO2/mCO2_ref)^k by linear regression: y = k * x
+    df = pd.read_csv("data/capture_costs.csv")
+    captured_ref = df["captured"].mean()
+    CAPEX_ref = df["CAPEX"].mean()
 
+    x = np.log(df["captured"] / captured_ref)
+    y = np.log(df["CAPEX"] / CAPEX_ref)
+    model = LinearRegression().fit(x.values.reshape(-1, 1), y.values)
+    k = model.coef_[0]
+
+    print(f"\nFitted model: CAPEX ≈ {CAPEX_ref:.2f} * (captured / {captured_ref:,.0f})^{k:.4f}")
+    print("Celsio waste-CCS (FOV) has reported CAPEX of 3715 MNOK @400 ktCO2/yr \n -> Demonstrasjon av Fullskala CO2-Håndtering - Rapport for Avsluttet Forprosjekt\n")
+    print("Use this^ to estimate CAPEX (includes C&L) - then add energy OPEX etc.!")
+    
+    # reading data from the present study and running EPR function
     plants = pd.read_csv("data/plants.csv")
     output = WACCUS_EPR(plants=plants, case="CCUS")
 
